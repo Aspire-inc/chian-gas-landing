df0bd331958e8d5cf6a3631dd6b4f504563dcac0
Emmanuel Essien <xxcel4ryll@gmail.com>
2023-06-21 04:49:13 +0100

    Production script added

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+
+require('dotenv').config();
+
+module.exports = {
+  apps: [
+    {
+      name: `Chian_Gas_FrontEnd_${process.argv[5]}`,
+      // exec_mode: 'cluster',
+      instances: 1, // Or 'max'
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start',
+      env_development: {
+        APP_ENV: 'Development',
+        PORT: '8000',
+      },
+      env_production: {
+        APP_ENV: 'Production',
+        PORT: '3000',
+      }
+    },
+  ],
+};
