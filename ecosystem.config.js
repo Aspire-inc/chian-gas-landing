462deb12f7354aa5b0a859a9ff2373208f926929
Emmanuel Essien <xxcel4ryll@gmail.com>
2023-06-24 10:18:33 +0100

    process manager config updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,14 +4,14 @@ require('dotenv').config();
 module.exports = {
   apps: [
     {
-      name: `Chian_Gas_Landing_Production`,
+      name: `Chian_Gas_Landing_${process.argv[5]}`,
       // exec_mode: 'cluster',
       instances: 1, // Or 'max'
       script: 'node_modules/next/dist/bin/next',
       args: 'start',
       env_development: {
         APP_ENV: 'Development',
-        PORT: '8000',
+        PORT: 8000,
       },
       env_production: {
         APP_ENV: 'Production',
