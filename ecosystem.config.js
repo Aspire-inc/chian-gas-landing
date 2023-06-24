0eb48e89c5b4b3ef282e708120b22371a98abe36
Emmanuel Essien <xxcel4ryll@gmail.com>
2023-06-24 08:49:24 +0100

    pm2 ecosystem modified

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,6 +9,7 @@ module.exports = {
       instances: 1, // Or 'max'
       script: 'node_modules/next/dist/bin/next',
       args: 'start',
+      watch: true,
       env_development: {
         APP_ENV: 'Development',
         PORT: 8000,
