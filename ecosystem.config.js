5546603bc3d07b5535aef77250324984b7afe318
Emmanuel Essien <xxcel4ryll@gmail.com>
2023-06-28 17:31:36 +0100

    pm2 modified

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       args: 'start',
       env_development: {
         APP_ENV: 'Development',
-        PORT: 8000,
+        PORT: '8000',
       },
       env_production: {
         APP_ENV: 'Production',
